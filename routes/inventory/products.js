f038e60d56154173190e22ca8cd1dfee4f1ba0bd
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2024-03-27 02:48:09 -0400

    add ability to edit products

--- routes/inventory/products.js
+++ routes/inventory/products.js
@@ -4,6 +4,7 @@ const router = Router()
 const Product = require('../../models/Product')
 const asyncErrorHandler = require('../../utils/asyncErrorHandler')
 const Category = require('../../models/Category')
+const { validationResult, body } = require("express-validator")
 
 // read
 router.get('/', asyncErrorHandler(async (req, res) => {
@@ -25,13 +26,29 @@ router.post('/create', (req, res) => {
 })
 
 //update
-router.get('/:id/update', (req, res) => {
-  res.end('Item update not yet implemented')
-})
 
-router.post('/:id/update', (req, res) => {
-  res.end("Item update POST not yet implemented")
-})
+router.post(
+  '/:id/update',
+  body('productQuantity').notEmpty(),
+  body('productQuantity').isInt({min: 0}),
+  asyncErrorHandler(async (req, res) => {
+    const validationMessages = validationResult(req)
+    const productToUpdate = await Product.findById(req.params.id)
+    let categories = (await Category.find({})).map(category => category.url)
+    if(
+      !(["/inventory/products", ...categories].includes(req.body.submittedFrom))
+    ) {
+      res.redirect('/')
+    } else if (productToUpdate === null || !validationMessages.isEmpty()) {
+      res.redirect(req.body.submittedFrom)
+    } else {
+      productToUpdate.quantity = req.body.productQuantity
+      await productToUpdate.save()
+      res.redirect(req.body.submittedFrom)
+    }
+
+  })
+)
 
 //delete
 
@@ -39,8 +56,6 @@ router.post('/:id/delete', asyncErrorHandler(async (req, res) => {
   const productToDelete = Product.findById(req.params.id)
   let categories = await Category.find({})
   categories = categories.map(category => category.url)
-  console.log('submitted from', req.body.submittedFrom)
-  console.log(categories)
   if(
     !(["/inventory/products", ...categories].includes(req.body.submittedFrom))
   ) {
